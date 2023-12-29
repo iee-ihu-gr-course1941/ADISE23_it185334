d4fd7f73c84c0eb8dc41be03ed2b057c5eab25c3
GEOP04 <g.pyrgos20@gmail.com>
2023-12-29 11:47:35 +0200

    create checkFounded sql function

--- functions.sql
+++ functions.sql
@@ -15,10 +15,10 @@ BEGIN
     
     WHILE counter < max_count DO
         IF (orientation_ = 'v') THEN
-    	    UPDATE board SET ship_on_board = 1 WHERE x_data = (x_ + counter) and (y_data = y_ ) and (player = playerPlayed);
+    	    UPDATE board SET ship_on_board = shipName WHERE x_data = (x_ + counter) and (y_data = y_ ) and (player = playerPlayed);
         END IF;
         IF (orientation_ = 'h') THEN
-    	    UPDATE board SET ship_on_board = 1 WHERE (x_data = x_) and (y_data = y_  + counter) and (player = playerPlayed);
+    	    UPDATE board SET ship_on_board = shipName WHERE (x_data = x_) and (y_data = y_  + counter) and (player = playerPlayed);
         END IF;
         SET counter = counter + 1;
     END WHILE;
@@ -35,4 +35,22 @@ BEGIN
 END //
 
 
-call placeShip('air_carrier',1,1,'h',1);
+create procedure checkFounded(IN ship_name varchar(255), IN player_ INT)
+BEGIN
+	declare hitted_count INT DEFAULT 0;
+    declare ship_lentgh INT DEFAULT 0;
+    
+    set hitted_count := (select count(*) from board where (board.ship_on_board = ship_name) and (board.is_hitted = 'Y') and (board.player = player_));
+    set ship_lentgh := (select lenght from ships_p1 WHERE ships_p1.typeofship = ship_name);
+    
+    IF hitted_count = ship_lentgh THEN
+    	select 'Y' as message;
+    ELSE
+    	select 'N' as message;
+    END IF;
+    
+END //
+
+call placeShip('air_carrier',0,0,'h',1);
+
+call checkFounded('air_carrier',1);
